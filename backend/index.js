fe4027d8702e54070bdf2f3cf9b87a433151c89b
Sobran Rajak <sobran0621@gmail.com>
2024-12-06 14:18:55 +0530

    add cors

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const db = mysql.createConnection({
 
 app.use(express.json({ limit: "500kb" })); // Set JSON payload limit to 500 KB
 app.use(cors({
-    origin: 'http://yourfrontenddomain.com',
+    origin: 'http://localhost:3000',
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
