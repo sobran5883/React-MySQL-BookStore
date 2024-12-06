d21da164ec7bc76eb83163340fe81630c778345d
Sobran Rajak <sobran0621@gmail.com>
2024-12-06 15:14:33 +0530

    cors edit

--- backend/index.js
+++ backend/index.js
@@ -17,11 +17,13 @@ const db = mysql.createConnection({
 });
 
 app.use(express.json({ limit: "500kb" })); // Set JSON payload limit to 500 KB
-app.use(cors({
-    // origin: 'https://react-my-sql-book-store.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors(
+//     {
+//     // origin: 'https://react-my-sql-book-store.vercel.app',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization']
+// }
+));
 
 
 // Test the database connection
